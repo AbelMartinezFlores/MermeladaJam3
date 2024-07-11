90072cc27f119b1def0171afd6f450df22b57563
gbd3 <71724054+gbd3@users.noreply.github.com>
2024-07-11 20:47:36 +0200

    Opciones Juego
    Estructura menú de opciones y funcionalidades de volver al menú principal implementada.

--- Assets/Scripts/Juego/IrAlMenu.cs
+++ Assets/Scripts/Juego/IrAlMenu.cs
@@ -1,11 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class IrAlJuego : MonoBehaviour{
-
+public class IrAlMenu : MonoBehaviour{
+    
     //1 Es la escena del juego, 0 es la escena del menu principal
     //Esto sirve para cambiar la escena del juego
-    public void CambiarDeEscena(){
-        SceneManager.LoadScene(1);
+    public void VolverAlMenuPrincipal(){
+        SceneManager.LoadScene(0);
     }
 }
