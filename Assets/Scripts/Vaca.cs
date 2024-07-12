a38f9e656b81e110dc9ed5543c2faa50e224b8a0
AbelMartinezFlores <abelmartinezlamola@gmail.com>
2024-07-12 16:28:41 +0200

    Dinero vaca

--- Assets/Scripts/Vaca.cs
+++ Assets/Scripts/Vaca.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Vaca : MonoBehaviour
 {
@@ -9,7 +10,39 @@ public class Vaca : MonoBehaviour
     [SerializeField] private GameObject cuerpo;
     private Stack<Ingrediente> comida;
 
+    private int dinero = 100;
+    [SerializeField] private Text textoDinero;
 
+    private void Start()
+    {
+        textoDinero.text = dinero.ToString();
+    }
+
+    public void SumarDinero(int c)
+    {
+        dinero += c;
+        textoDinero.text = dinero.ToString();
+    }
+
+    public void RestarDinero(int c)
+    {
+        dinero -= c;
+        if (dinero < 0) dinero = 0;
+        textoDinero.text = dinero.ToString();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SumarDinero(100);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            RestarDinero(20);
+        }
+    }
 
 
 }
