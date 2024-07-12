dccc86eb9abe2bada381b7a057b1bbed821f1f7f
Nerea-L <nerea.llorens.martinez@gmail.com>
2024-07-12 19:59:33 +0200

    Inventario
    Da un error

--- Assets/Scripts/Juego/Ingredientes/Inventario.cs
+++ Assets/Scripts/Juego/Ingredientes/Inventario.cs
@@ -6,8 +6,12 @@ using System;
 
 public class Inventario : MonoBehaviour
 {
+    public GameObject prefabInventario;
+    public List<Objeto> objetos = new List<Objeto>();
 
-    public class ItemInventario: IComparable<ItemInventario>
+    private Objeto obj;
+
+    /*public class ItemInventario: IComparable<ItemInventario>
     {
         public string objeto;
         public int cantidad;
@@ -16,29 +20,28 @@ public class Inventario : MonoBehaviour
         {
             objeto = obj;
             cantidad = n;
-
         }
 
         public int CompareTo(ItemInventario other)
         {
             return this.objeto.CompareTo(other.objeto);
         }
-    }
+    }*/
 
-    private List<ItemInventario> objetos = new List<ItemInventario>();
 
-    public List<ItemInventario> getObjetos() { return objetos; }
+
+    public List<Objeto> getObjetos() { return objetos; }
 
     public void QuitarObjeto(string newItem)
     {
-
         for (int i = 0; i < objetos.Count; i++)
         {
-            if (objetos[i].objeto.CompareTo(newItem) == 0)
+            if (objetos[i].nombre.text == newItem)
             {
-                objetos[i].cantidad--;
+                int aux = int.Parse(objetos[i].cantidad.text) - 1;
 
-                if (objetos[i].cantidad <= 0) objetos.RemoveAt(i);
+                if (aux <= 0) objetos.RemoveAt(i);
+                else objetos[i].UpdateIngrediente(aux);
 
                 break;
             }
@@ -46,25 +49,52 @@ public class Inventario : MonoBehaviour
 
     }
 
-    public void AgregarObjeto(string newItem)
+    private void Start()
     {
+        Ingrediente i1 = ScriptableObject.CreateInstance<Ingrediente>();
+        i1.nombre = "a";
+        i1.sprite = Resources.Load<Sprite>("../../Sprites/LaVacabeza.png"); ;
+        
+        GameObject inventario = GameObject.Instantiate(prefabInventario, Vector2.zero, Quaternion.identity, GameObject.FindGameObjectWithTag("Inventario").transform);
+        obj = inventario.GetComponent<Objeto>();
+        obj.CrearIngrediente(i1, 2);
 
+        objetos.Add(obj);
+
+        Ingrediente i2 = ScriptableObject.CreateInstance<Ingrediente>();
+        i2.nombre = "b";
+        i2.sprite = Resources.Load<Sprite>("../../Sprites/LaVacacuerpo.png");
+        
+        GameObject inventario2 = GameObject.Instantiate(prefabInventario, Vector2.zero, Quaternion.identity, GameObject.FindGameObjectWithTag("Inventario").transform);
+        obj = inventario2.GetComponent<Objeto>();
+        obj.CrearIngrediente(i2, 5);
+
+        objetos.Add(obj);
+    }
+
+    public void AgregarObjeto(Ingrediente ing)
+    {
         bool existe = false;
-
 
         for(int i = 0; i< objetos.Count; i++)
         {
-            if( objetos[i].objeto.CompareTo(newItem) == 0)
+            if (objetos[i].nombre.text == ing.nombre)
             {
-                objetos[i].cantidad++;
+                int aux = int.Parse(objetos[i].cantidad.text) + 1;
+                objetos[i].UpdateIngrediente(aux);
                 existe = true;
+
                 break;
             }
         }
 
-        if( !existe)
+        if(!existe)
         {
-            objetos.Add(new ItemInventario(newItem, 1));
+            GameObject inventario = GameObject.Instantiate(prefabInventario, Vector2.zero, Quaternion.identity, GameObject.FindGameObjectWithTag("Inventario").transform);
+            obj = inventario.GetComponent<Objeto>();
+            obj.CrearIngrediente(ing, 1);
+
+            objetos.Add(obj);
             objetos.Sort();
         }
         
@@ -72,9 +102,15 @@ public class Inventario : MonoBehaviour
 
     public void MostrarObjetos()
     {
+       // ScriptableObject objetoEncontrado = new Ingrediente();
+
         for (int i = 0; i < objetos.Count; i++)
         {
-            Debug.Log(objetos[i].objeto + ": " + objetos[i].cantidad);
+            
+            /*GameObject obj = GameObject.Instantiate(prefabInventario, Vector2.zero, Quaternion.identity, GameObject.FindGameObjectWithTag("Inventario").transform);
+            obj.GetComponent<SpriteRenderer>().sprite = objetoEncontrado.sprite;*/
+
+            //Debug.Log(objetos[i].objeto + ": " + objetos[i].cantidad);
         }
     }
 
