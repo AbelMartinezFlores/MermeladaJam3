7bd24b015e84c77eb33fa771616dd086155445be
AbelMartinezFlores <abelmartinezlamola@gmail.com>
2024-07-13 13:03:18 +0200

    cambios animacion

--- Assets/Animations/Objeto Mezclado/AnimacionObjetoMezclado.cs
+++ Assets/Animations/Objeto Mezclado/AnimacionObjetoMezclado.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AnimacionObjetoMezclado : MonoBehaviour
 {
 
     private Animator animator;
+    [SerializeField] private Image fondo;
+    [SerializeField] private Image objeto;
+    [SerializeField] private Text nombre;
+    [SerializeField] private Text dinero;
+
 
 
     private void Awake()
@@ -13,22 +19,18 @@ public class AnimacionObjetoMezclado : MonoBehaviour
         animator = gameObject.GetComponent<Animator>();
     }
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-
-            Aparecer();
-        }
-    }
-
     public void cambiarEscena()
     {
 
     }
 
-    public void Aparecer()
+    public void Aparecer(Sprite obj, Sprite fon, string nom, int din)
     {
+        nombre.text = nom;
+        dinero.text = din.ToString();
+        fondo.sprite = fon;
+        objeto.sprite = obj;
+        
         animator.SetTrigger("Aparecer");
     }
 
