fb6d916f15125a0aab265b68f56ee4e88d826f1c
AbelMartinezFlores <abelmartinezlamola@gmail.com>
2024-07-14 16:50:25 +0200

    Hover botones y arreglar hover texto

--- Assets/Scripts/Comunes/HoverRatonTexto.cs
+++ Assets/Scripts/Comunes/HoverRatonTexto.cs
@@ -2,13 +2,17 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class HoverRatonTexto : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class HoverRatonTexto : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
 
     [SerializeField] private Color normal;
     [SerializeField] private Color hover;
     [SerializeField] private Text texto;
 
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        texto.color = normal;
+    }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
