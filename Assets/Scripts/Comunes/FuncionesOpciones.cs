8152f287d02eaded5e7e39306455072dc49b6569
gbd3 <71724054+gbd3@users.noreply.github.com>
2024-07-15 12:22:43 +0200

    Pantalla Completa
    Ahora el botón de pantalla completa se actualiza

--- Assets/Scripts/Comunes/FuncionesOpciones.cs
+++ Assets/Scripts/Comunes/FuncionesOpciones.cs
@@ -3,13 +3,24 @@ using UnityEngine.UI;
 
 public class FuncionesOpciones : MonoBehaviour{
     public Text textoMarcaBoton;
+
+    //Cambia el estado inicial del boton segun como este la pantalla
+    void Start(){
+        if(Screen.fullScreen){
+            textoMarcaBoton.text = "X";
+        }else{
+            textoMarcaBoton.text = "";
+        }
+    }
+
+    //Cambia entre pantalla completa y ventana y actualiza el recuadro donde se muestra
     public void CambiarAPantallaCompleta(){
         if(Screen.fullScreen){
-            Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, false);
             textoMarcaBoton.text = "";
+            Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, false);
         }else{
-            Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, true);
             textoMarcaBoton.text = "X";
+            Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, true);
         }
     }
 }
